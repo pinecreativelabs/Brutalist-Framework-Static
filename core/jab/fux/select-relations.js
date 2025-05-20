64b575ef1f977773269afd3e811f3f381cd4c4cf
Pine Creative Labs <brad.fogelstrom@gmail.com>
2025-05-20 16:16:50 -0400

    2.5.4

--- core/jab/fux/select-relations.js
+++ core/jab/fux/select-relations.js
@@ -8,38 +8,30 @@ function SelectRelations() {
     document.addEventListener('DOMContentLoaded', function () {
         initializeFiltering();
     });
-
     function initializeFiltering() {
         const selectRelations = document.querySelectorAll('.select-relations');
-
         selectRelations.forEach(select => {
             // COLD START
             updateFiltering(select, false);
-
             // Check if Select2 is initialized
             const isSelect2Initialized = $(select).hasClass('select2') !== undefined;
-
             // Add event listener for change event
             select.addEventListener('change', function () {
                 updateFiltering(this);
             });
-
             // If Select2 is initialized, also listen to its change event
             if (isSelect2Initialized) {
                 $(select).on('change.select2', function () {
                     updateFiltering(this);
                 });
             }
-
         });
         // Reset parent selects' selected options if they are hidden due to filtering
         resetParentSelects();
     }
-
     function updateFiltering(select, restart = true) {
         const parentId = select.getAttribute('id');
         document.querySelectorAll(`[data-sf-parent*="${parentId}"]`).forEach(childSelect => {
-
             if (childSelect.tagName == "SELECT")
                 // SELECT INPUT
                 childSelect.querySelectorAll('option').forEach(option => {
@@ -49,7 +41,6 @@ function SelectRelations() {
                             const [selectId, selectValues] = pair.split(':');
                             var el_parent = document.getElementById(selectId);
                             var ids = selectValues.split(',');
-
                             if (el_parent.type == "checkbox")
                                 return ids.includes(el_parent.checked ? '1' : '0')
                             else
@@ -68,7 +59,6 @@ function SelectRelations() {
                         const [selectId, selectValues] = pair.split(':');
                         var el_parent = document.getElementById(selectId);
                         var ids = selectValues.split(',');
-
                         if (el_parent.type == "checkbox")
                             return ids.includes(el_parent.checked ? '1' : '0')
                         else
@@ -82,16 +72,12 @@ function SelectRelations() {
         if (restart)
             resetParentSelects();
     }
-
     function resetParentSelects() {
         document.querySelectorAll(`[data-sf-parent]`).forEach(childSelect => {
-
             if (childSelect.tagName == "SELECT") {
                 // SELECT INPUT
-
                 if (childSelect.selectedIndex == null || childSelect.selectedIndex == -1)
                     return;
-
                 let selectOption = childSelect.options[childSelect.selectedIndex];
                 if (selectOption.disabled) {
                     $(childSelect).val(null).trigger('change.select2');
@@ -100,5 +86,4 @@ function SelectRelations() {
         });
     }
 }
-// autorun
 SelectRelations(); //Init
