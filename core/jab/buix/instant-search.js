f3076bc600e4c23b36caf955d8020a6f7220da69
Pine Creative Labs <brad.fogelstrom@gmail.com>
2026-04-21 14:43:36 -0400

    2.7

--- core/jab/buix/instant-search.js
+++ core/jab/buix/instant-search.js
@@ -1,45 +1,21 @@
-let cards = document.querySelectorAll('.box')  
+let cards = document.querySelectorAll('.searchable')  
 function liveSearch() {
-    let search_query = document.getElementById("searchbox").value;
+    let search_query = document.getElementById("instant-search").value;
     
     //Use innerText if all contents are visible
     //Use textContent for including hidden elements
     for (var i = 0; i < cards.length; i++) {
         if(cards[i].textContent.toLowerCase()
                 .includes(search_query.toLowerCase())) {
-            cards[i].classList.remove("is-hidden");
+            cards[i].classList.remove("hidden");
         } else {
-            cards[i].classList.add("is-hidden");
+            cards[i].classList.add("hidden");
         }
     }
 }
-//A little delay
 let typingTimer;               
 let typeInterval = 500;  
-let searchInput = document.getElementById('searchbox');
-searchInput.addEventListener('keyup', () => {
-    clearTimeout(typingTimer);
-    typingTimer = setTimeout(liveSearch, typeInterval);
-});
-
-
-let boxes = document.querySelectorAll('.image') 
-function liveSearch() {
-    let search_query = document.getElementById("searchbox").value;
-
-    for (var i = 0; i < boxes.length; i++) {
-        if (boxes[i].getAttribute('alt').toLowerCase()
-            .includes(search_query.toLowerCase())) {
-            boxes[i].classList.remove("is-hidden");
-        } else {
-            boxes[i].classList.add("is-hidden");
-        }
-    }
-}
-//A little delay
-let typingTimer;
-let typeInterval = 500;
-let searchInput = document.getElementById('searchbox');
+let searchInput = document.getElementById('instant-search');
 searchInput.addEventListener('keyup', () => {
     clearTimeout(typingTimer);
     typingTimer = setTimeout(liveSearch, typeInterval);
