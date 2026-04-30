a299475239c6fd9ce6411c0d740199b52eaeb223
Pine Creative Labs <brad.fogelstrom@gmail.com>
2026-04-30 15:25:39 -0400

    2.7

--- core/jab/plugins/cookie-consent.js
+++ core/jab/plugins/cookie-consent.js
@@ -1,5 +1,5 @@
 !function(r){r.fn.grtCookie=function(t){var o,e,c,n=r.extend({textcolor:"#333",background:"#fff",buttonbackground:"#333",buttontextcolor:"#fff",duration:365},t);return-1<document.cookie.indexOf("acceptgrt=0")?this.remove():(this.css({color:n.textcolor,background:n.background}),r("span.cookie-btn").css({background:n.buttonbackground,color:n.buttontextcolor}),this.addClass("cookie-active"),o=new Date,e=n.duration,o.setTime(o.getTime()+24*e*60*60*1e3),c="expires="+o.toUTCString(),document.cookie="acceptgrt=1;"+c+";path=/",r(".cookie-btn").on("click",function(){r(this).parent().remove(),document.cookie="acceptgrt=0;"+c+";path=/"})),this}}(jQuery);
-/* Start the plugin
+// Start the plugin
 $(document).ready(function(){
 	$(".cookie-consent").grtCookie({
 		// Main text and background color, or apply core classes to HTML elements
@@ -12,4 +12,3 @@ $(document).ready(function(){
 		duration: 365,
 	});
 });
-*/
